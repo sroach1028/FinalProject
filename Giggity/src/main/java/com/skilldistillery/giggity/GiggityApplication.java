5dd67060d0d9f33dc4cade3c1e65c5d595ea7723
Shaun Roach <sroach1028@gmail.com>
2019-12-17 15:29:10 -0700

    added boot

---
+++ Giggity/src/main/java/com/skilldistillery/giggity/GiggityApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.giggity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiggityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiggityApplication.class, args);
+	}
+
+}
---
+++ Giggity/src/test/java/com/skilldistillery/giggity/GiggityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.giggity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiggityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
