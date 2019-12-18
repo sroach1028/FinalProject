386168f6dc5ba1c780aa8b7e92be6e39ef6c7ee9
tduplantis83 <tduplantis83@gmail.com>
2019-12-18 14:37:04 -0700

    fixed security

--- Giggity/src/main/java/com/skilldistillery/giggity/GiggityApplication.java
+++ Giggity/src/main/java/com/skilldistillery/giggity/GiggityApplication.java
@@ -2,12 +2,20 @@ package com.skilldistillery.giggity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class GiggityApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GiggityApplication.class, args);
+	}
+	
+	@Bean
+	public PasswordEncoder configurePasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
