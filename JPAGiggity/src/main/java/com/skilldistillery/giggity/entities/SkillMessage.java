bea7763dbe3eeb205780a42d778e897302a8b335
tduplantis83 <tduplantis83@gmail.com>
2019-12-17 17:27:46 -0700

    UserSkill, User, SkillMessage, Skill Entities stubbed out.

--- JPAGiggity/src/main/java/com/skilldistillery/giggity/entities/SkillMessage.java
+++ JPAGiggity/src/main/java/com/skilldistillery/giggity/entities/SkillMessage.java
@@ -1,5 +1,31 @@
 package com.skilldistillery.giggity.entities;
 
-public class SkillMessage {
+import java.time.LocalDateTime;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class SkillMessage {
+	// F I E L D S
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column(name = "poster_id")
+	private int posterId;
+
+	@Column(name = "skill_id")
+	private int skillId;
+
+	private String message;
+
+	@Column(name = "message_date")
+	private LocalDateTime messageDate;
+
+	@Column(name = "in_reply_to")
+	private int inReplyTo;
 }
--- JPAGiggity/src/main/java/com/skilldistillery/giggity/entities/UserSkill.java
+++ JPAGiggity/src/main/java/com/skilldistillery/giggity/entities/UserSkill.java
@@ -1,5 +1,15 @@
 package com.skilldistillery.giggity.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
 public class UserSkill {
+	// F I E L D S
+	@Column(name="user_skill_id")
+	private int userSkillId;
+	
+	@Column(name="image_id")
+	private int imageId;
 
 }
