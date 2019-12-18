fca2656ff6c7e59de828a1f5779b4edd3515e05d
Shaun Roach <sroach1028@gmail.com>
2019-12-17 17:36:17 -0700

    a few entities

--- JPAGiggity/src/main/java/com/skilldistillery/giggity/entities/BookingMessage.java
+++ JPAGiggity/src/main/java/com/skilldistillery/giggity/entities/BookingMessage.java
@@ -1,5 +1,20 @@
 package com.skilldistillery.giggity.entities;
 
-public class BookingMessage {
+import java.time.LocalDateTime;
 
+import javax.persistence.Column;
+
+public class BookingMessage {
+	private int id;
+	private int bookingId;
+	private String message;
+	
+	@Column(name="message_date")
+	private LocalDateTime messageDate;
+	
+	@Column(name="seller_id")
+	private int sellerid;
+
+	@Column(name="buyer_id")
+	private int buyerid;
 }
--- JPAGiggity/src/main/java/com/skilldistillery/giggity/entities/SellerReview.java
+++ JPAGiggity/src/main/java/com/skilldistillery/giggity/entities/SellerReview.java
@@ -1,5 +1,17 @@
 package com.skilldistillery.giggity.entities;
 
-public class SellerReview {
+import java.time.LocalDateTime;
 
+import javax.persistence.Column;
+
+public class SellerReview {
+	private int id;
+	private String comment;
+	private int rating;
+	
+	@Column(name="booking_id")
+	private int bookingid;
+	
+	@Column(name="review_date")
+	private LocalDateTime reviewDate;
 }
