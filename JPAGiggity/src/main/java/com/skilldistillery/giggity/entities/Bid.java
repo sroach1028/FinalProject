5c5a3fdb16efcc4cc76bf19111397572a111b203
Kullen <kullen.kee@outlook.com>
2019-12-17 17:34:14 -0700

    entitites baseline

--- JPAGiggity/src/main/java/com/skilldistillery/giggity/entities/Bid.java
+++ JPAGiggity/src/main/java/com/skilldistillery/giggity/entities/Bid.java
@@ -1,5 +1,27 @@
 package com.skilldistillery.giggity.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Bid {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private boolean available;
+
+	private String description;
+
+	@Column(name = "bidder_id")
+	private int bidderId;
+
+	@Column(name = "job_id")
+	private int jobId;
+
+	private boolean accepted;
 
 }
