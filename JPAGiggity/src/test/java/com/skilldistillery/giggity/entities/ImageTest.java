eea46758a42fcd9dbcdd1304358d5f5538aabe8a
Kullen <kullen.kee@outlook.com>
2019-12-18 09:30:02 -0700

    test baseline

---
+++ JPAGiggity/src/test/java/com/skilldistillery/giggity/entities/ImageTest.java
@@ -0,0 +1,48 @@
+package com.skilldistillery.giggity.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ImageTest {
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Image image;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("GiggityPU");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		image = em.find(Image.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		image = null;
+	}
+
+	@Test
+	void test() {
+		assertNotNull(image);
+	}
+
+}
