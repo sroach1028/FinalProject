a9768e8f4952125e3f1c2c017bff05c690ddbdd8
Shaun Roach <sroach1028@gmail.com>
2019-12-18 16:49:02 -0700

    changes to bidcontroller

--- Giggity/src/main/java/com/skilldistillery/giggity/repositories/BidRepo.java
+++ Giggity/src/main/java/com/skilldistillery/giggity/repositories/BidRepo.java
@@ -1,9 +1,11 @@
 package com.skilldistillery.giggity.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.skilldistillery.giggity.entities.Bid;
 
 public interface BidRepo extends JpaRepository<Bid, Integer> {
-
+	List<Bid> findByJob_Id(int id);
 }
--- Giggity/src/main/java/com/skilldistillery/giggity/services/BidSvcImpl.java
+++ Giggity/src/main/java/com/skilldistillery/giggity/services/BidSvcImpl.java
@@ -1,10 +1,13 @@
 package com.skilldistillery.giggity.services;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.skilldistillery.giggity.entities.Bid;
 import com.skilldistillery.giggity.repositories.BidRepo;
 
 @Service
@@ -13,5 +16,15 @@ public class BidSvcImpl implements BidService {
 	
 	@Autowired
 	BidRepo bidRepo;
+
+	@Override
+	public List<Bid> getBidsByJobId(Integer jobId) {
+		List<Bid> bids = bidRepo.findByJob_Id(jobId);
+		if (bids != null) {
+			return bids;
+		}
+		else
+			return null;
+	}
 
 }
