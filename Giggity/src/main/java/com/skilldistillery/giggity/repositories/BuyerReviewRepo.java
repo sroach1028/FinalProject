01086f620c5a47d670880fba725725ccf919fabe
Kvothik <cromeans15@gmail.com>
2019-12-19 15:55:28 -0700

    made buyer review repo/service methods

--- Giggity/src/main/java/com/skilldistillery/giggity/repositories/BuyerReviewRepo.java
+++ Giggity/src/main/java/com/skilldistillery/giggity/repositories/BuyerReviewRepo.java
@@ -1,14 +1,12 @@
 package com.skilldistillery.giggity.repositories;
 
-import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.skilldistillery.giggity.entities.BuyerReview;
 
 public interface BuyerReviewRepo extends JpaRepository<BuyerReview, Integer> {
-//	public BuyerReview findById(int id);
-//	public List<BuyerReview> findByRating(int rating);
-//	public List<BuyerReview> findByJob_Id(int id);
-//	public List<BuyerReview> findByJob_TitleLike(String title);
+	public BuyerReview findById(int id);
+	public BuyerReview findByJob_Id(int id);
+	public BuyerReview findByJob_TitleLike(String title);
 }
