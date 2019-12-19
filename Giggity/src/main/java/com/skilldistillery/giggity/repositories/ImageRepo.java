ff926ed677a2beaf9a23b01f396a819ed1b673c8
tduplantis83 <tduplantis83@gmail.com>
2019-12-18 20:53:44 -0700

    Filled Repos with lots of queries

--- Giggity/src/main/java/com/skilldistillery/giggity/repositories/ImageRepo.java
+++ Giggity/src/main/java/com/skilldistillery/giggity/repositories/ImageRepo.java
@@ -1,9 +1,16 @@
 package com.skilldistillery.giggity.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.skilldistillery.giggity.entities.Image;
 
 public interface ImageRepo extends JpaRepository<Image, Integer> {
-
+	public Image findById(int id);
+	public List<Image> findByJobs_Id(int id);
+	public List<Image> findByJobs_TitleLike(String title);
+	public List<Image> findBySkills_Id(int id);
+	public List<Image> findBySkills_NameLike(String skill);
+	
 }
--- Giggity/src/main/java/com/skilldistillery/giggity/repositories/JobRepo.java
+++ Giggity/src/main/java/com/skilldistillery/giggity/repositories/JobRepo.java
@@ -7,9 +7,20 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.skilldistillery.giggity.entities.Job;
 
 public interface JobRepo extends JpaRepository<Job, Integer> {
-	Job findById(int id);
-	List<Job> findByAddress_CityLike(String city);
-	List<Job> findByTitle(String title);
-	List<Job> findBySkill_Name(String name);
-	
+	public Job findById(int id);
+	public List<Job> findByTitleLike(String title);
+	public List<Job> findByAddress_CityLike(String city);
+	public List<Job> findByAddress_State(String city);
+	public List<Job> findByAddress_Zip(int zip);
+	public List<Job> findBySkill_NameLike(String name);
+	public List<Job> findByRemote(Boolean remote);
+	public List<Job> findByUser_Id(int id);
+	public List<Job> findByUser_UsernameLike(String username);
+	public List<Job> findByUser_EmailLike(String email);
+	public List<Job> findByBookings_Id(int id);
+	public List<Job> findByBuyerReviews_Rating(int rating);
+	public List<Job> findByJobBids_Id(int id);
+	public List<Job> findByBookingMessages_Id(int id);
+	public List<Job> findByBookingMessages_MessageLike(String message);
+	public List<Job> findByJobImages_Id(int id);
 }
