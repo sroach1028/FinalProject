1dedc07f72880c253e47bd8e2257035bac013777
tduplantis83 <tduplantis83@gmail.com>
2019-12-20 10:27:17 -0700

    Added User to Routing module

--- ngGiggity/src/app/app-routing.module.ts
+++ ngGiggity/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { UserComponent } from './components/user/user.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { RegisterComponent } from './components/register/register.component';
 import { NgModule } from '@angular/core';
@@ -10,6 +11,7 @@ import { SearchResultsComponent } from './components/search-results/search-resul
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: HomeComponent },
+  { path: 'user', component: UserComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'search', component: SearchResultsComponent },
