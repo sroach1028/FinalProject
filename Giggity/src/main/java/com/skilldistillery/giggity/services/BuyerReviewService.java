5df053d25aa86814d942f4afcb868150305165e9
tduplantis83 <tduplantis83@gmail.com>
2019-12-19 17:21:04 -0700

    Buyer Review Repo, Svc, Impl, and Controller done and Tested.

--- Giggity/src/main/java/com/skilldistillery/giggity/services/BuyerReviewService.java
+++ Giggity/src/main/java/com/skilldistillery/giggity/services/BuyerReviewService.java
@@ -1,11 +1,17 @@
 package com.skilldistillery.giggity.services;
 
+import java.util.List;
+
 import com.skilldistillery.giggity.entities.BuyerReview;
 
 public interface BuyerReviewService {
 
-	public BuyerReview findByJobId(int jid);
+	public BuyerReview findById(int id);
+	
+	public BuyerReview findByJobId(int id);
 
+	public List<BuyerReview> findByJobTitle(String title);
+	
 	public BuyerReview create(BuyerReview buyerReview);
 
 	public BuyerReview update(int bid, BuyerReview buyerReview);
