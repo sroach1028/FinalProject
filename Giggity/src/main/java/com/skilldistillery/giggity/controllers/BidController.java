735b54e614ce0ba16c531b044c1e878009a97d5f
Shaun Roach <sroach1028@gmail.com>
2019-12-19 17:32:51 -0700

    kljdfhg

--- Giggity/src/main/java/com/skilldistillery/giggity/controllers/BidController.java
+++ Giggity/src/main/java/com/skilldistillery/giggity/controllers/BidController.java
@@ -1,5 +1,6 @@
 package com.skilldistillery.giggity.controllers;
 
+import java.security.Principal;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
@@ -8,11 +9,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.skilldistillery.giggity.entities.Bid;
+import com.skilldistillery.giggity.entities.User;
 import com.skilldistillery.giggity.services.BidService;
+import com.skilldistillery.giggity.services.JobService;
+import com.skilldistillery.giggity.services.UserService;
 
 @RestController
 @RequestMapping(path = "api")
@@ -21,6 +27,10 @@ public class BidController {
 	
 	@Autowired
 	BidService bidSvc;
+	@Autowired
+	JobService jobSvc;
+	@Autowired
+	UserService userSvc;
 	
 	@GetMapping("bids/{jid}")
 	public List<Bid> getBids(@PathVariable Integer jid, HttpServletResponse res) {
@@ -32,5 +42,18 @@ public class BidController {
 			res.setStatus(200);
 		return bids;
 	}
-
+	
+	@PostMapping("bids/{jid}")
+	public Bid addBid(@RequestBody Bid bid, @PathVariable Integer jid, HttpServletResponse res, Principal principal) {
+		User bidder = userSvc.getUserByUsername(principal.getName());
+		if (jobSvc.getById(jid) == null) {
+			res.setStatus(404);
+		}
+		else {
+		bid.setJob(jobSvc.getById(jid));
+		bid.setBidder(bidder);
+		bid = bidSvc.addBid(bid);
+		}
+		return bid;
+	}
 }
