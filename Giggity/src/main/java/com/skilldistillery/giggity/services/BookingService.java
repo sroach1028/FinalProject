9ad70f0a095769827b69c256b2dd7cbc3344bdb9
Kvothik <cromeans15@gmail.com>
2019-12-23 15:38:46 -0700

    working on booking repo

--- Giggity/src/main/java/com/skilldistillery/giggity/services/BookingService.java
+++ Giggity/src/main/java/com/skilldistillery/giggity/services/BookingService.java
@@ -6,13 +6,13 @@ import com.skilldistillery.giggity.entities.Booking;
 
 public interface BookingService {
 	
-	public List<Booking> getActive(int uid);
-	
-	public List<Booking> getTransactionHistory(int uid);
+//	public List<Booking> getActive(int uid);
+//	
+//	public List<Booking> getTransactionHistory(int uid);
 	
 	public List<Booking> getAllByJob(int jid);
 	
-	public List<Booking> getAllByUser(int uid);
+//	public List<Booking> getAllByUser(int uid);
 	
 	public Booking create(Booking booking);
 
