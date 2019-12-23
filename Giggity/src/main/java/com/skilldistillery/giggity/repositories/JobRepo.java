343242676903eb0d7f45f7d3b068a3b40efaef9d
Shaun Roach <sroach1028@gmail.com>
2019-12-23 15:32:33 -0700

    edited job repo and conroller

--- Giggity/src/main/java/com/skilldistillery/giggity/repositories/JobRepo.java
+++ Giggity/src/main/java/com/skilldistillery/giggity/repositories/JobRepo.java
@@ -20,8 +20,6 @@ public interface JobRepo extends JpaRepository<Job, Integer> {
 	public List<Job> findByBookings_Id(int id);
 	public List<Job> findByBuyerReviews_Rating(int rating);
 	public List<Job> findByJobBids_Id(int id);
-	public List<Job> findByBookingMessages_Id(int id);
-	public List<Job> findByBookingMessages_MessageLike(String message);
 	public List<Job> findByJobImages_Id(int id);
 	public List<Job> findByUser_Username(String username);
 }
