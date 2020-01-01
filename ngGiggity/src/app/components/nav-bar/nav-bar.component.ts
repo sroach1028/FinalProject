92353b410455ecd9511536bd2cbb9a6fdde1be05
tduplantis83 <tduplantis83@gmail.com>
2020-01-01 12:36:40 -0700

    Updated Nav-Bar to show logged in user's name

--- ngGiggity/src/app/components/nav-bar/nav-bar.component.ts
+++ ngGiggity/src/app/components/nav-bar/nav-bar.component.ts
@@ -5,6 +5,7 @@ import { UserService } from './../../services/user.service';
 import { AuthService } from 'src/app/services/auth.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-nav-bar',
@@ -16,20 +17,39 @@ export class NavBarComponent implements OnInit {
   constructor(private authSvc: AuthService, private usersvc: UserService, private router: Router) { }
 
   searchDisplay = 'user';
+  userSelected: User = null;
+  userFullName: string = null;
 
   searchBy(event: any) {
     this.searchDisplay = event.target.value;
   }
 
   isLoggedIn() {
+    if (this.authSvc.checkLogin() && this.userFullName === null) {
+      this.getUser();
+    }
     return this.authSvc.checkLogin();
   }
+
   logOut() {
     this.authSvc.logout();
+    this.userFullName = null;
   }
 
   ngOnInit() {
 
+  }
+
+  getUser() {
+    this.usersvc.getUserByUsername().subscribe(
+      data => {
+        this.userSelected = data;
+        this.userFullName = this.userSelected.firstName + ' ' + this.userSelected.lastName;
+      },
+      err => console.error('Get User Error in Nav-Bar Component.ts')
+    );
+
+    this.userSelected = null;
   }
 
 }
