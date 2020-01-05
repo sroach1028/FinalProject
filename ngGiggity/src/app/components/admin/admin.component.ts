74e31ea0073aa21b2515862d135766081fe7ff8b
Kullen <kullen.kee@outlook.com>
2020-01-05 16:05:22 -0700

    admin refurbished

--- ngGiggity/src/app/components/admin/admin.component.ts
+++ ngGiggity/src/app/components/admin/admin.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
 import { User } from 'src/app/models/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin',
@@ -10,25 +11,37 @@ import { User } from 'src/app/models/user';
 export class AdminComponent implements OnInit {
   users: User[];
   username: string;
-  allUsers = false;
 
-  constructor(private usersvc: UserService) { }
+  constructor(private usersvc: UserService, private router: Router) { }
 
   ngOnInit() {
+
+    this.showUsers();
   }
 
 
 
   showUsers() {
+
     this.usersvc.getAllUsers().subscribe(
       data => {
+        for (let i = 0; i < data.length; i++) {
+
+          if (data[i].username === 'admin') {
+            data.splice(i, 1);
+          }
+
+        }
         this.users = data;
       },
       err => {
         console.error('Update Job error in User Compnent');
+        this.router.navigateByUrl('/home');
       }
     );
-    this.allUsers = true;
+
+    // if (this.username !== 'admin') {
+    // }
 
 
 
@@ -40,6 +53,8 @@ export class AdminComponent implements OnInit {
 
       data => {
         console.log(data);
+        // tslint:disable-next-line: prefer-for-of
+
       },
       err => {
         console.error('Error disabling user');
